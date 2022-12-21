fe9fbb21f166dffae0c2585541e4a6406ee5321e
tzu.chieh.lin <tzu.chieh.lin@deltaww.com>
2022-12-21 10:45:17 +0800

    implment Toolbar

--- src/pages/ProductDetailPage/Toolbar.jsx
+++ src/pages/ProductDetailPage/Toolbar.jsx
@@ -1,12 +1,64 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+import React from "react";
+import PropTypes from "prop-types";
+import FlexBox from "../../components/FlexBox";
+import styled from "styled-components";
+import { ShoppingCartOutlined } from "@ant-design/icons";
+import { Button, Empty, Popover } from "antd";
 
-function Toolbar(props) {
+const Container = styled(FlexBox)`
+  border: 1px gray solid;
+`;
+
+const PopoverContent = ({ selectedProds, onRemove }) =>
+  selectedProds.length ? (
+    selectedProds.map((item) => {
+      const handleRemove = () => {
+        onRemove(item?.id);
+      };
+      return (
+        <FlexBox key={item?.id}>
+          <FlexBox row>
+            {item?.img}
+            {item?.name}
+          </FlexBox>
+          <Button onClick={handleRemove}>Remove from cart</Button>
+        </FlexBox>
+      );
+    })
+  ) : (
+    <Empty />
+  );
+function Toolbar({ selectedProds, onRemove }) {
   return (
-    <div>Toolbar</div>
-  )
+    <Container row justify="flex-end">
+      <Popover
+        trigger="click"
+        placement="bottomRight"
+        content={
+          <PopoverContent selectedProds={selectedProds} onRemove={onRemove} />
+        }
+      >
+        <ShoppingCartOutlined />
+      </Popover>
+    </Container>
+  );
 }
 
-Toolbar.propTypes = {}
+Toolbar.defaultProps = {
+  selectedProds: [],
+  onRemove: () => {},
+};
 
-export default Toolbar
+Toolbar.propTypes = {
+  selectedProds: PropTypes.arrayOf(
+    PropTypes.objectOf({
+      id: PropTypes.number,
+      img: PropTypes.node,
+      name: PropTypes.string,
+    })
+  ),
+
+  onRemove: PropTypes.func,
+};
+
+export default Toolbar;
